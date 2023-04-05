6377eb8a0765150f9286d3adc7c5053a7178de56
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-04-05 13:23:01 -0400

    Finished verb/adjective/noun and bugs endpoints.

--- app.js
+++ app.js
@@ -5,9 +5,31 @@ const express = require("express");
 const app = express();
 
 //Routes
-app.get("/", (req, res) => {
-    res.send("Congratulations on starting a new project called")
+app.get("/:verb/:adjective/:noun", (req, res) => {
+    const {verb, adjective, noun} = req.params;
+    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
+});
+app.get("/bugs/:numberOfBugs", (req, res) => {
+    const {numberOfBugs} = req.params;
+    if (Number(numberOfBugs) >= 200){
+        res.send(`
+        <h2>Too many bugs!! Start over!</h2>
+        <a href="/bugs">Start Over</a>
+        `)
+    } else {
+        
+        res.send(`
+        <h2>${numberOfBugs} little bugs in the code</h2>
+        <a href="/bugs/${Number(numberOfBugs) + 2}">Pull one down, patch it around</a>
+        `)
+    }
 })
+app.get("/bugs", (req, res) => {
+    res.send(`
+    <h2>99 little bugs in the code</h2>
+    <a href="/bugs/101">Pull one down, Patch it around</a>
+    `)
+});
 
 
 // EXPORT
