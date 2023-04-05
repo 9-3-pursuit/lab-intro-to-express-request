b5bc8bfb3dcfb3e73bfbbbe42112f55369e56dd8
amber-king <amberking@pursuit.org>
2023-04-05 13:42:07 -0400

    added on bugs route

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 // Dependencies
-const express = require("express")
+const express = require("express");
 const pokemon = require("./models/pokemon.json");
 // console.log(pokemon[0]);
 
 // Configuration
-const app = express()
+const app = express();
 
 // Routes
 app.get("/", (req, res) => {
-    res.send("Welcome 99 Pokemon 🪰")
-})
+  res.send("Welcome 99 Pokemon 🪰");
+});
 
-app.get("/:verb/:adjective/:noun", (req,res) => {
-    const {verb, adjective, noun} = req.params
-    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}`)
-})
+// Name Generator -> 3 parameters in the URL
+app.get("/:verb/:adjective/:noun", (req, res) => {
+  const { verb, adjective, noun } = req.params;
+  res.send(
+    `Congratulations on starting a new project called ${verb}-${adjective}-${noun} !`
+  );
+});
+
+//99 Little Bugs
+app.get("/bugs", (req, res) => {
+  res.send(
+    `<h1>99 little bugs in the code</h1> 
+    <a herf=/bugs/101> pull one down, patch it around<a>`
+  );
+});
+
+// number of bugs -> both > 200 & > 200
+app.get("/bugs/:numberOfBugs", (req, res) => {
+  const { numberOfBugs } = req.params;
+  if (numberOfBugs >= 200) {
+    res.send("Start over!");
+  } else {
+    res.send(
+      `<a href='/bugs/${
+        +numberOfBugs + 2
+      }'> Pull one down, patch it around </a> ${numberOfBugs} little bugs in the code`
+    );
+  }
+});
