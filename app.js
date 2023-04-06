f33df5565b77d061b8007063847fa655747c3eb7
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-06 09:50:42 -0400

    Complete Lab

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const pokemon = require('./models/pokemon.json');
+
+function capitalize(str) {
+    const capitalizedStr = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    return capitalizedStr;
+}
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome 99 Pokemon");
+})
+app.get('/:verb/:adjective/:noun', (req, res) => {
+    const { verb, adjective, noun } = req.params;
+    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
+})
+
+app.get('/bugs', (req, res) => {
+    res.send(
+      `<h1>99 little bugs in the code</h1><a href="/bugs/101">pull one down, patch it around</a>`
+    );
+})
+
+app.get('/bugs/:numberOfBugs', (req, res) => {
+    const { numberOfBugs } = req.params;
+    if (numberOfBugs < 200 ) {
+        res.send(`<h1>${numberOfBugs} little bugs in the code</h1><a href="/bugs/${Number(numberOfBugs) + 2}">Pull one down, patch it around</a>`);
+    } else {
+        res.send(`<a href="/bugs">Too many bugs!! Start over!</a>`);
+    }
+})
+
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon)
+})
+
+
+app.get('/pokemon/search', (req, res) => {
+    const { name } = req.query;
+    const pokemonName = capitalize(name)
+    const foundPokemon = pokemon.find(poke => poke.name === pokemonName)
+    if (foundPokemon) {
+        res.send([foundPokemon])
+    } else {
+        res.send([])
+    }
+})
+
+app.get('/pokemon/:index', (req, res) => {
+    const { index } = req.params;
+    if (pokemon[index]) {
+        res.send(pokemon[index]);
+    } else {
+        res.send(`Sorry, no pokemon found at ${index}`)
+    }
+})
+module.exports = app;
