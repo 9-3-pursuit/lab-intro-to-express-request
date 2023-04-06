bcc6b26eb4e8918657b3c36c02834b0e639282d1
Fernando Cortez <cortez.fernando11216@gmail.com>
2023-04-06 17:34:18 -0400

    fixed indexOfArray

--- app.js
+++ app.js
@@ -4,42 +4,44 @@ const pokemon = require('./models/pokemon');
 console.log(pokemon[0]); 
 
 
-app.get('/bugs', (req, res) => { 
-    res.send("99 little bugs in the code"); 
-});
-
- 
-app.get('/bugs/:number_of_bugs', (req, res) => {    
-
-     req.params.numberOfBugs
-    if (req.params.number_of_bugs > 200) {
-        res.send("Too many bugs!! Start over!");
-    } else {
-        res.send(`${req.params.number_of_bugs} little bugs in the code <a href="/bugs/${Number(req.params.number_of_bugs) + 2}">Pull one down, patch it around</a>`);
-    }
-}); 
-
-
-
 app.get('/', (req, res) => {
     res.send("Welcome 99 Pokemon");
 } ); 
 
+app.get('/bugs', (req, res) => { 
+    res.send(`<h1>99 little bugs in the code</h1><br> <a href="/bugs/101"> Pull one down, patch it around</a>`); 
+
+} ); 
+
+app.get('/bugs/:number_Of_Bugs', (req, res) => {    
+    const NumOfBugs  = req.params.number_Of_Bugs;
+    if (NumOfBugs < 200) {
+        res.send(`${NumOfBugs} little bugs in the code <a href="/bugs/${Number(NumOfBugs) + 2}">Pull one down, patch it around</a>`);
+   } else {
+    res.send(`Too many bugs!! Start over! <a href="/bugs"> Click Here </a>`);
+   }
+});
+
+app.get('/pokemon', (req, res) => { 
+    res.json(pokemon);
+} ); 
 
 app.get('/pokemon/:indexOfArray', (req, res) => { 
     res.send(pokemon[req.params.indexOfArray]); 
 } );
 
 
-app.get('/pokemon/search', (req, res) => {  
+app.get('/pokemon/:search', (req, res) => {  
     res.json(pokemon.filter(poke => poke.name.toLowerCase() === req.query.name.toLowerCase())); 
 } );
-
-app.get('/pokemon', (req, res) => { 
-    res.json(pokemon);
-} ); 
+ 
 
 
+app.get('/:verb/:adjective/:noun', (req, res) => {
+    const {verb,adjective,noun} = req.params
+    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
+  
+  })
 
 
 module.exports = app;
