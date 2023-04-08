636b17d4316f33c8d24471b7c161da387988f47f
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-08 04:02:36 -0400

    add welcome

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ app.get("/", (req, response) => {
   response.send("Starting a new project");
 });
 
+app.get("/:verb/:adjective/:noun", (req, res) => {
+  const verb = req.params.verb;
+  const adjective = req.params.adjective;
+  const noun = req.params.noun;
+  const message = `Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`;
+  res.send(message);
+});
+
 module.exports = app;
