e5a4f96e8a9d7a12c724e5e0c960d329d6d3d80e
touhami-ben <brooklyn.ben76@gmail.com>
2023-04-08 15:51:36 -0400

    add Bonus

--- app.js
+++ app.js
@@ -81,12 +81,44 @@ app.get("/pokemon/search", (req, res) => {
       res.send(`Sorry, no pokemon found at ${indexOfArray}`);
     }
   });
+
+  // BONUS
+
+  //create new route and send unordered list of anchor tags that link to the array position of the pokemon
+function renderPokemonList(pokemon) {
+    let list = "<ul>"
+   list = pokemon
+      .map((poke, i) => {
+        return `<li><a href="/pokemon-pretty/${i}">${poke.name}</a></li>`;
+      }).join("");
+ //styling the page 
+    list += "</ul><style>body {background-color: green}</style>";
+    return list;
+    // console.log(list)
+  }
   
   
+  app.get("/pokemon-pretty", (req, res) => {
+    res.send(renderPokemonList(pokemon));
+  });
+  //display the name, image and any other info of each individual poke
+  function individualPokemon(individualPokemon) {
+    const nameStr = `<h2>${individualPokemon.name}</h2>`;
+    const imgStr = `<img src="${individualPokemon.img}" alt="${individualPokemon.name}"></img>`;
+    return nameStr + imgStr;
+  }
+  
+  app.get("/pokemon-pretty/:index", (req, res) => {
+    const { index } = req.params;
+    res.send(individualPokemon(pokemon[index]));
+  });
+  
+  
+  
 
 
 
 
   
-
+//EXPORTS
 module.exports = app;
