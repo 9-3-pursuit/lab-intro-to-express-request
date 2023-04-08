0b3bd00ea58c3571295e40c976aaa83120e2e60d
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-08 00:41:42 -0400

    add express function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (request, response) => {
+app.get("/", (req, response) => {
   response.send("Starting a new project");
 });
+
+module.exports = app;
