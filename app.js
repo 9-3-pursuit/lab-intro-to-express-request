7d06726863e17989953c82b08af7a4e70d7b730b
Karimah Reavis <Karimah586@gmail.com>
2023-04-09 12:14:33 -0400

    mostly working

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.get("/bugs", (request, response) => {
+    response.send("<h1>99 little bugs in the code</h1><br><a href=localhost:8888/bugs/101>pull one down, patch it around</a>")
+})
+
+app.get("/bugs/101", (req,res) => {
+    const {101} = req.params;
+    res.send(`${101} little bugs in the code`)
+})
+
 app.get("/", (request, response) => {
     response.send("Welcome to express words app!")
 })
