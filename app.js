591f7696318f2306ea869b67d091ca3046901204
HaroldF415 <harold.f415@gmail.com>
2023-04-08 23:07:59 -0400

    three params added

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ const app = express();
 app.get("/", (req, res) => {
   res.status(200).send("Home Page");
 });
+
+app.get("/:param1/:param2/:param3", (req, res) => {
+  const { param1, param2, param3 } = req.params;
+  res.send(`Congratulations on starting a new project called ${param1}-${param2}-${param3}!`);
+});
+
 module.exports = app;
