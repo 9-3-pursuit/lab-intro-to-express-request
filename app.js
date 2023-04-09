7911c449db480cd93bc0beab8a811cc2303f9f41
HaroldF415 <harold.f415@gmail.com>
2023-04-09 00:43:57 -0400

    orignal logic

--- app.js
+++ app.js
@@ -4,18 +4,14 @@ const app = express();
 
 const pokemon = require("./models/pokemon.json");
 
-app.get("/", (req, res) => {
-  res.status(200).send("Welcome 99 Pokemon");
-});
+const rootHandler = (req, res) => {
+  res.status(200).send(`Welcome 99 Pokemon`);
+};
 
-app.get("/:verb/:adjective/:noun", (req, res) => {
+const verbAdjectiveNounHandler = (req, res) => {
   const { verb, adjective, noun } = req.params;
-  res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`);
-});
-
-app.get("/bugs", (req, res) => {
-  res.send("99 little bugs in the code");
-});
+  res.status(200).send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`);
+};
 
 const tooManyBugsTemplate = () => `
   <h1>Too many bugs!! Start over!</h1>
@@ -27,37 +23,59 @@ const bugsTemplate = (numOfBugs, nextNumOfBugs) => `
   <a href="/bugs/${nextNumOfBugs}">Pull one down, patch it around</a>
 `;
 
-app.get("/bugs/:numberOfBugs", (req, res) => {
+const bugsHandler = (req, res) => {
+  res.status(200).send(`99 little bugs in the code`);
+};
+
+const tooManyBugsHandler = (req, res) => {
   const numOfBugs = Number(req.params.numberOfBugs);
   const nextNumOfBugs = numOfBugs + 2;
 
   if (numOfBugs >= 200) {
-    res.send(tooManyBugsTemplate());
+    res.status(501).send(tooManyBugsTemplate());
   } else {
-    res.send(bugsTemplate(numOfBugs, nextNumOfBugs));
+    res.status(200).send(bugsTemplate(numOfBugs, nextNumOfBugs));
   }
-});
+};
 
-app.get("/pokemon", (req, res) => {
+const pokemonHandler = (req, res) => {
   res.status(200).json(pokemon);
-});
+};
 
-app.get("/pokemon/search", (req, res) => {
+const pokemonSearchHandler = (req, res) => {
   const pokemonName = req.query.name;
   const pokemonFound = pokemon.filter((poke) => {
     return poke.name.toLowerCase() === pokemonName.toLowerCase();
   });
-  res.json(pokemonFound);
-});
 
-app.get("/pokemon/:index", (req, res) => {
+  if (!pokemonFound) {
+    res.status(404).json(pokemonFound);
+  }
+  res.status(200).json(pokemonFound);
+};
+
+const pokemonIndexHandler = (req, res) => {
   const indexOfPokemon = Number(req.params.index);
 
   if (!pokemon[indexOfPokemon]) {
-    res.send(`Sorry, no pokemon found at ${indexOfPokemon}`);
+    res.status(404).send(`Sorry, no pokemon found at ${indexOfPokemon}`);
   }
 
-  res.json(pokemon[indexOfPokemon]);
-});
+  res.status(200).json(pokemon[indexOfPokemon]);
+};
+
+app.get("/", rootHandler);
+
+app.get("/:verb/:adjective/:noun", verbAdjectiveNounHandler);
+
+app.get("/bugs", bugsHandler);
+
+app.get("/bugs/:numberOfBugs", tooManyBugsHandler);
+
+app.get("/pokemon", pokemonHandler);
+
+app.get("/pokemon/search", pokemonSearchHandler);
+
+app.get("/pokemon/:index", pokemonIndexHandler);
 
 module.exports = app;
