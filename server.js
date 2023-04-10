e4b2383b8d439245fc97fbe636e0548360655806
Tafari Excell <excell.tafari@gmail.com>
2023-04-10 09:23:44 -0400

    server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
 require('dotenv').config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT 
 console.log('PORT: ', PORT);
 
 app.listen(PORT, () => {
