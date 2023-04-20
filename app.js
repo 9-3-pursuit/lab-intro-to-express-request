e7e41dab0b183d169eba8315ee75d5fed9da04fa
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-04-19 21:00:12 -0400

    Third test satisfied.

--- app.js
+++ app.js
@@ -6,6 +6,34 @@ app.get("/", (req, res) => {
 });
 
 app.get("/bugs", (req, res) => {
-  res.send(`<h1>99 little bugs in the code</h1>`);
+  res.send(
+    `<h1>99 little bugs in the code</h1>
+    <span>99 little bugs in the code,</span>
+    <span>99 little bugs,</span>
+    <a href="/bugs/101">
+        <span>pull one down,</span>
+        <span>patch it around,</span>
+    </a>
+    <span>101 bugs in the code.</span>`
+  );
+});
+
+app.get("/bugs/:numberOfBugs", (req, res) => {
+  if (req.params.numberOfBugs <= 199) {
+    res.send(
+      `<h1>${req.params.numberOfBugs} little bugs in the code</h1>
+                <span>${req.params.numberOfBugs} little bugs in the code,</span>
+                <span>${req.params.numberOfBugs} little bugs,</span>
+                <a href="/bugs/${parseInt(req.params.numberOfBugs) + 2}">
+                    <span>pull one down,</span>
+                    <span>patch it around,</span>
+                </a>
+                <span>${
+                  parseInt(req.params.numberOfBugs) + 2
+                } bugs in the code.</span>`
+    );
+  } else {
+    res.send(`<h1>Too many bugs!! Start over!</h1>`);
+  }
 });
 module.exports = app;
