8fbb34ef1599032b0a428d68f4e6d0c42b2eb41f
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-04-19 20:37:58 -0400

    Satisfy second test.

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ app.get("/", (req, res) => {
   res.send("Welcome 99 Pokemon");
 });
 
+app.get("/bugs", (req, res) => {
+  res.send(`<h1>99 little bugs in the code</h1>`);
+});
 module.exports = app;
