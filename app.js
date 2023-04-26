467ec63d7ca41df1b3af10192b054b4d757ac2d9
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-04-26 19:48:55 -0400

    FINISHED

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const pokemon = require("./models/pokemon.json");
+
 app.get("/", (req, res) => {
   res.send("Welcome 99 Pokemon");
 });
@@ -21,19 +23,44 @@ app.get("/bugs", (req, res) => {
 app.get("/bugs/:numberOfBugs", (req, res) => {
   if (req.params.numberOfBugs <= 199) {
     res.send(
-      `<h1>${req.params.numberOfBugs} little bugs in the code</h1>
-                <span>${req.params.numberOfBugs} little bugs in the code,</span>
-                <span>${req.params.numberOfBugs} little bugs,</span>
-                <a href="/bugs/${parseInt(req.params.numberOfBugs) + 2}">
-                    <span>pull one down,</span>
-                    <span>patch it around,</span>
-                </a>
-                <span>${
-                  parseInt(req.params.numberOfBugs) + 2
-                } bugs in the code.</span>`
+      `
+      ${parseInt(req.params.numberOfBugs)} little bugs in the code
+        <a href="/bugs/${
+          parseInt(req.params.numberOfBugs) + 2
+        }">Pull one down, patch it around</a>
+      `
     );
   } else {
     res.send(`<h1>Too many bugs!! Start over!</h1>`);
   }
 });
+
+app.get("/pokemon", (req, res) => {
+  res.send(pokemon);
+});
+
+app.get("/pokemon/search", (req, res) => {
+  let r = pokemon.filter((entry) => {
+    return (
+      entry.name.trim().toLowerCase() === req.query.name.trim().toLowerCase()
+    );
+  });
+  console.log(r);
+  res.send(r);
+});
+
+app.get("/pokemon/:index", (req, res) => {
+  if (pokemon[req.params.index]) {
+    res.send(pokemon[req.params.index]);
+  } else {
+    res.send(`Sorry, no pokemon found at ${req.params.index}`);
+  }
+});
+
+app.get("/:gerund/:adjective/:noun", (req, res) => {
+  res.send(
+    `Congratulations on starting a new project called ${req.params.gerund}-${req.params.adjective}-${req.params.noun}!`
+  );
+});
+
 module.exports = app;
